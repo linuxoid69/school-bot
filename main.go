ff175367a114437e4c517fc99653e77142422de5
Rustam Tagaev <rustam.tagaev@avroid.tech>
2024-09-14 11:37:59 +0300

    []  create message and send

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -33,6 +35,12 @@ type Site struct {
 	DateTo     string
 }
 
+type Message struct {
+	Text   string `json:"text"`
+	ChatID string `json:"chat_id"`
+	Token  string
+}
+
 func main() {
 	data, err := GetGrades(
 		&Site{
@@ -46,7 +54,21 @@ func main() {
 		slog.Warn("Error getting grades", "error", err)
 	}
 
-	CreateMessage(data)
+	messageString, err := CreateMessage(data)
+	if err != nil {
+		slog.Warn("Error creating message", "error", err)
+	}
+
+	mesg := Message{
+		Text:   messageString,
+		ChatID: os.Getenv("CHAT_ID"),
+		Token:  os.Getenv("TOKEN"),
+	}
+
+	err = mesg.SendGrades()
+	if err != nil {
+		slog.Warn("Error sending message", "error", err)
+	}
 }
 
 func GetGrades(site *Site) ([]byte, error) {
@@ -55,9 +77,9 @@ func GetGrades(site *Site) ([]byte, error) {
 		return nil, err
 	}
 
-	req.Header.Set("Cookie", fmt.Sprintf("X-JWT-Token=%s", site.JWT))
-
 	todayDate := time.Now().Format("02.01.2006")
+
+	req.Header.Set("Cookie", fmt.Sprintf("X-JWT-Token=%s", site.JWT))
 
 	q := req.URL.Query()
 	q.Add("p_educations[]", fmt.Sprintf("%s", site.EucationID))
@@ -80,25 +102,58 @@ func GetGrades(site *Site) ([]byte, error) {
 	return body, nil
 }
 
-func CreateMessage(data []byte) {
+func CreateMessage(data []byte) (string, error) {
 	var grades Grades
 
 	json.Unmarshal(data, &grades)
+
+	var sb strings.Builder
+
+	sb.WriteString(fmt.Sprintf(
+		"Оценки за %s: \n%s\n",
+		time.Now().Format("02.01.2006"),
+		"========================================"))
 
 	for _, item := range grades.Data.Items {
 		if item.EstimateComment == nil {
 			item.EstimateComment = ""
 		}
 
-		fmt.Printf("\nУрок: %s \nДата: %s \nИтог: %s \nГде: %s \nКомментарий: %s \n----------------------------------",
-			item.SubjectName,
-			item.Date,
-			item.EstimateValueName,
-			item.EstimateTypeName,
-			item.EstimateComment)
+		_, err := sb.WriteString(
+			fmt.Sprintf("\nУрок: %s \nИтог: %s \nГде: %s \nКомментарий: %s \n%s",
+				item.SubjectName,
+				item.EstimateValueName,
+				item.EstimateTypeName,
+				item.EstimateComment,
+				"-------------------------------------------------------"))
+		if err != nil {
+			return "", err
+		}
 	}
+
+	return sb.String(), nil
 }
 
-func SendGrades() {
+func (m *Message) SendGrades() error {
+	payload, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
 
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", m.Token),
+		bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
